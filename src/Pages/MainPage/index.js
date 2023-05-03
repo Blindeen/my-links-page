b3d3e8320b46a71fe536cc1354738e252dad5841
Blindeen <kuba.szmajda@gmail.com>
2023-05-03 15:21:39 +0200

    format files using prettier

--- src/Pages/MainPage/index.js
+++ src/Pages/MainPage/index.js
@@ -1,48 +1,46 @@
-import {Col, Row} from "antd";
-import {SocialIcon} from 'react-social-icons';
+import { Col, Row } from 'antd';
+import { SocialIcon } from 'react-social-icons';
 
-import {CardCol, StyledImage, StyledIconCol, LayoutRow, SocialsRow} from "./styles";
+import { CardCol, StyledImage, StyledIconCol, LayoutRow, SocialsRow } from './styles';
 
 const MainPage = () => {
-    const socialItems = [
-        'https://github.com/Blindeen',
-        'https://twitter.com/kubaszmajda',
-        'https://www.instagram.com/kubaszmajda/',
-        'https://www.linkedin.com/in/jakub-szmajda-773742265/',
-        'https://www.facebook.com/kubaszmajda/',
-        'https://open.spotify.com/user/kszmajda?si=d7adbf43f4ed4319',
-    ]
+  const socialItems = [
+    'https://github.com/Blindeen',
+    'https://twitter.com/kubaszmajda',
+    'https://www.instagram.com/kubaszmajda/',
+    'https://www.linkedin.com/in/jakub-szmajda-773742265/',
+    'https://www.facebook.com/kubaszmajda/',
+    'https://open.spotify.com/user/kszmajda?si=d7adbf43f4ed4319',
+  ];
 
-    const socialItemsJSX = socialItems.map((link, index) => (
-        <Col key={index} xs={{span: 24}} lg={{span: 4}}>
-            <Row key={index}>
-                <StyledIconCol key={index}>
-                    <SocialIcon key={index} url={link} target="_blank" fgColor='white' bgColor='transparent'/>
-                </StyledIconCol>
+  const socialItemsJSX = socialItems.map((link, index) => (
+    <Col key={index} xs={{ span: 24 }} lg={{ span: 4 }}>
+      <Row key={index}>
+        <StyledIconCol key={index}>
+          <SocialIcon key={index} url={link} target='_blank' fgColor='white' bgColor='transparent' />
+        </StyledIconCol>
+      </Row>
+    </Col>
+  ));
+
+  return (
+    <LayoutRow>
+      <CardCol xs={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
+        <Row>
+          <StyledImage xs={{ span: 24 }} lg={{ span: 12, offset: 6 }} />
+        </Row>
+        <SocialsRow>
+          <Col xs={{ span: 24 }} lg={{ span: 16, offset: 4 }}>
+            <Row>
+              <Col span={16} offset={4}>
+                <Row>{socialItemsJSX}</Row>
+              </Col>
             </Row>
-        </Col>
-    ))
+          </Col>
+        </SocialsRow>
+      </CardCol>
+    </LayoutRow>
+  );
+};
 
-    return (
-        <LayoutRow>
-            <CardCol xs={{span: 24}} lg={{span: 12, offset: 6}}>
-                <Row>
-                    <StyledImage xs={{span: 24}} lg={{span: 12, offset: 6}}/>
-                </Row>
-                <SocialsRow>
-                    <Col xs={{span: 24}} lg={{span: 16, offset: 4}}>
-                        <Row>
-                            <Col span={16} offset={4}>
-                                <Row>
-                                    {socialItemsJSX}
-                                </Row>
-                            </Col>
-                        </Row>
-                    </Col>
-                </SocialsRow>
-            </CardCol>
-        </LayoutRow>
-    )
-}
-
-export default MainPage
+export default MainPage;
--- src/Pages/MainPage/styles.js
+++ src/Pages/MainPage/styles.js
@@ -1,5 +1,5 @@
-import styled from "styled-components"
-import {Col, Row} from "antd";
+import styled from 'styled-components';
+import { Col, Row } from 'antd';
 
 export const LayoutRow = styled(Row)`
   margin-top: 50px;
@@ -7,16 +7,14 @@ export const LayoutRow = styled(Row)`
   @media (max-width: 480px) {
     margin-top: 0;
   }
-`
+`;
 
 export const SocialsRow = styled(Row)`
-    margin-top: 20px;
-`
+  margin-top: 20px;
+`;
 
 export const StyledImage = styled(Col)`
-  background-image:
-          linear-gradient(to bottom, transparent 80%, black),
-          url('/assets/winter_photo_web.jpg');
+  background-image: linear-gradient(to bottom, transparent 80%, black), url('/assets/winter_photo_web.jpg');
   height: 458px;
   background-size: cover;
   margin-bottom: 25px;
@@ -25,21 +23,21 @@ export const StyledImage = styled(Col)`
   @media (max-width: 480px) {
     border-radius: 0;
   }
-`
+`;
 
 export const CardCol = styled(Col)`
   background-color: black;
   border-radius: 12px;
   padding: 40px 0 25px 0;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  
+
   @media (max-width: 480px) {
     border-radius: 0;
     padding: 0;
   }
-`
+`;
 
 export const StyledIconCol = styled(Col)`
   width: fit-content;
   margin: 0 auto 0 auto;
-`
+`;
