a09fcfe21b93a986cb97dd9b5284494b33bb46ce
Blindeen <kuba.szmajda@gmail.com>
2023-05-27 19:20:22 +0200

    export social links

---
+++ src/socialLinks.js
@@ -0,0 +1,30 @@
+import palette from './palette';
+
+const socialLinks = [
+  {
+    link: 'https://github.com/Blindeen',
+    color: palette.github,
+  },
+  {
+    link: 'https://twitter.com/kubaszmajda',
+    color: palette.twitterBlue,
+  },
+  {
+    link: 'https://www.instagram.com/kubaszmajda/',
+    color: palette.instagramPink,
+  },
+  {
+    link: 'https://www.linkedin.com/in/jakub-szmajda-773742265/',
+    color: palette.linkedinBlue,
+  },
+  {
+    link: 'https://www.facebook.com/kubaszmajda/',
+    color: palette.facebookBlue,
+  },
+  {
+    link: 'https://open.spotify.com/user/kszmajda?si=d7adbf43f4ed4319',
+    color: palette.spotifyGreen,
+  },
+];
+
+export default socialLinks;
